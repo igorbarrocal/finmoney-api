0efcbe0fdacd63bb63367db994fd40375a1a5808
igorbarrocal <igorbarrocal@hotmail.com>
2025-08-14 09:23:03 -0300

    Salvando o Projeto

---
+++ src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
@@ -0,0 +1,30 @@
+package br.com.fiap.fin_money_api.repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import br.com.fiap.fin_money_api.model.Transaction;
+import br.com.fiap.fin_money_api.model.TransactionType;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction>{
+    
+    //SELECT * FROM TRANSACTION WHERE DESCRIPTION = :DESCRIPTION
+    //@Query("SELECT t FROM TRANSACTION as t WHERE t.description = :description") //JPQL
+    //Page<Transaction> findByDescriptionContainingIgnoringCase(String description, Pageable pageable); // Query Methods
+
+
+
+    //Page<Transaction> findByDate(LocalDate date, Pageable pageable);
+
+
+
+    Page<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date, Pageable pageable);
+
+    List<Transaction> findByType(TransactionType type);
+    
+}
